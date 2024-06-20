6bf389ce6a9ed5c5d4f5b3c713f7601064096211
Batuhan <batuhangumus08@gmail.com>
2024-06-20 19:21:27 +0300

    change on code

--- Code by python/code.py
+++ Code by python/code.py
@@ -1,27 +1,38 @@
 print("Starting")
 
 import board
-import supervisor
-import board
-import digitalio
-import storage
-import usb_cdc
-import usb_hidW
 
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
+from kmk.modules.layers import Layers
+from kmk.extensions.media_keys import MediaKeys
+
 
 keyboard = KMKKeyboard()
+keyboard.extensions.append(MediaKeys())
+keyboard.modules.append(Layers())
 
-keyboard.col_pins = (board.GP10, board.GP11,board.GP12,board.GP13) # Cols
-keyboard.row_pins = (board.GP27, board.GP28,board.GP29)             # Rows
+keyboard.col_pins = (board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18, board.GP19, board.GP20, board.GP21, board.GP22, board.GP26, board.GP27, board.GP28)
+keyboard.row_pins = (board.GP6, board.GP7, board.GP8, board.GP9, board.GP10)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# Keymap
 keyboard.keymap = [
-    [KC.W, KC.A, KC.B, KC.C, KC.D, KC.E,KC.F, KC.G, KC.H, KC.J, KC.K, KC.L]
-]
+    [       #0
+        KC.ESC, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC, KC.GRV,
+        KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLS, KC.DEL,
+        KC.CAPS, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.NO, KC.ENT, KC.PGUP,
+        KC.LSFT, KC.NO, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RSFT, KC.UP, KC.PGDN,
+        KC.LCTRL, KC.LGUI, KC.LALT, KC.NO, KC.NO, KC.NO, KC.SPC, KC.NO, KC.NO, KC.RALT, KC.MO(1), KC.RCTRL, KC.LEFT, KC.DOWN, KC.RIGHT
+    ],
 
+    [
+        KC.TRNS, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.MUTE,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.P5, KC.VOLU,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.P8, KC.VOLD,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.P4, KC.P2, KC.P6
+    ]
+]
 if __name__ == '__main__':
     keyboard.go()
